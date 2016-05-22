fa2e6e7b8a42f32d37378c7a046379a85462764d
David Steinsland <david@davidsteinsland.net>
2016-05-23 00:10:53 +0200

    Fixed usermod/groupmod

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,31 +2,31 @@
 
 set -e
 
-until ip addr show dev tun0 2>/dev/null;
-do
-  sleep 1
-done
-
-if [ $EUID -eq 0 -a "$1" = '/transmission-daemon.sh' ];
+if [ ! -z "${OPENVPN_USERNAME}" ];
 then
-  if [ ! -z "${TRANSMISSION_GROUP_ID}" ];
+  until ip addr show dev tun0 &>/dev/null;
+  do
+    sleep 1
+  done
+fi
+
+if [ $(id -u) -eq 0 -a "$1" = '/transmission-daemon.sh' ];
+then
+  if [ ! -z "${TRANSMISSION_USER_ID}" ];
   then
     echo "Changing GID to $TRANSMISSION_GROUP_ID"
     #groupmod -g $TRANSMISSION_GROUP_ID -o transmission
-    delgroup transmission
-    addgroup -g $TRANSMISSION_GROUP_ID -S transmission
-  fi
-
-  if [ ! -z "${TRANSMISSION_USER_ID}" ];
-  then
-    echo "Changing UID to $TRANSMISSION_USER_ID"
-    #usermod -u $TRANSMISSION_USER_ID -o transmission
     deluser transmission
-    adduser -S -u $TRANSMISSION_USER_ID -G transmission transmission
+    #delgroup transmission
+    addgroup -S -g $TRANSMISSION_GROUP_ID transmission
+
+    echo "Changing UID to $TRANSMISSION_USER_ID"
+    adduser -S -G transmission -u $TRANSMISSION_USER_ID transmission
   fi
 
   chown -R transmission:transmission /etc/transmission
   chown -R transmission:transmission /config
+  chown -R transmission:transmission /data
 
   exec gosu transmission "$@"
 fi
