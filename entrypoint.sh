e2e99ea0e3c4dde91000c9710cf84cf8c9bbc3a3
David Steinsland <david@davidsteinsland.net>
2016-05-22 23:28:44 +0200

    Fixed usermod

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,17 +9,24 @@ done
 
 if [ $EUID -eq 0 -a "$1" = '/transmission-daemon.sh' ];
 then
-  if [ ! -z "${TRANSMISSION_USER_ID}" ];
-  then
-    echo "Changing UID to $TRANSMISSION_USER_ID"
-    usermod -u $TRANSMISSION_USER_ID -o transmission
-  fi
-
   if [ ! -z "${TRANSMISSION_GROUP_ID}" ];
   then
     echo "Changing GID to $TRANSMISSION_GROUP_ID"
-    groupmod -g $TRANSMISSION_GROUP_ID -o transmission
+    #groupmod -g $TRANSMISSION_GROUP_ID -o transmission
+    delgroup transmission
+    addgroup -g $TRANSMISSION_GROUP_ID -S transmission
   fi
+
+  if [ ! -z "${TRANSMISSION_USER_ID}" ];
+  then
+    echo "Changing UID to $TRANSMISSION_USER_ID"
+    #usermod -u $TRANSMISSION_USER_ID -o transmission
+    deluser transmission
+    adduser -S -u $TRANSMISSION_USER_ID -G transmission transmission
+  fi
+
+  chown -R transmission:transmission /etc/transmission
+  chown -R transmission:transmission /config
 
   exec gosu transmission "$@"
 fi
