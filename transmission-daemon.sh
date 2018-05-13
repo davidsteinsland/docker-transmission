10f458ede89ad3e9464fba26fef7f315ef9def1a
David Steinsland <125661+davidsteinsland@users.noreply.github.com>
2018-05-13 11:56:08 +0200

    Updated port forwarding with the new API
    See https://www.privateinternetaccess.com/forum/discussion/23431/new-pia-port-forwarding-api for more information

--- transmission-daemon.sh
+++ transmission-daemon.sh
@@ -53,8 +53,11 @@ then
 
   echo 'Loading port forward assignment information..'
 
-  _POST_DATA="user=$OPENVPN_USERNAME&pass=$OPENVPN_PASSWORD&client_id=$PIA_CLIENT_ID&local_ip=$TRANSMISSION_BIND_ADDRESS_IPV4"
-  _PIA_RESPONSE=$(wget --post-data="$_POST_DATA" -O - 'https://www.privateinternetaccess.com/vpninfo/port_forward_assignment')
+  _PIA_RESPONSE=$(curl "http://209.222.18.222:2000/?client_id=$PIA_CLIENT_ID" 2>/dev/null)
+  if [ "${_PIA_RESPONSE}" == "" ]; then
+    echo "Port forwarding is already activated on this connection, has expired, or you are not connected to a PIA region that supports port forwarding"
+    exit 1
+  fi
   export TRANSMISSION_PEER_PORT=$(echo $_PIA_RESPONSE | head -1 | grep -oE "[0-9]+")
 fi
 
